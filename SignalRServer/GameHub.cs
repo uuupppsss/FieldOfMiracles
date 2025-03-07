644ba815a47d2b0ecd73d3ece89e138c6ff23906
uuupppsss <ulya.ch.06@mail.ru>
2025-03-07 12:54:02 +1000

    Добавьте файлы проекта.

---
+++ SignalRServer/GameHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer
+{
+    public class GameHub:Hub
+    {
+
+        public GameHub()
+        {
+            
+        }
+
+        static Dictionary<string, ISingleClientProxy> clientsByNickname
+
+        public async Task SendLetter(string letter)
+        {
+
+        }
+
+        public async Task GuessWord(string word)
+        {
+
+        }
+
+
+    }
+}
---
+++ SignalRServer/GameState.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace SignalRServer
+{
+    public class GameState
+    {
+        public string Answer {  get; set; }
+        public HashSet<char> GuessedLetters { get; set; }
+        public GameState(string answer) 
+        {
+            Answer=answer.ToLower();
+            GuessedLetters = new HashSet<char>();
+        }
+
+        public string GetCurrentState()
+        {
+            var displayedWord = new StringBuilder();
+            foreach (var letter in Answer)
+            {
+                if(GuessedLetters.Contains(letter)) displayedWord.Append(letter);
+                else displayedWord.Append("_");
+            }
+            return displayedWord.ToString();
+        }
+
+        public bool GuessLetter(char letter)
+        {
+            if (!GuessedLetters.Contains(letter))
+            {
+                GuessedLetters.Add(letter);
+                return true; //буква угадана
+            }
+            else return false; //буква уже угадана
+        }
+
+        public bool IfWordGuessed()
+        {
+            return Answer.All(c => GuessedLetters.Contains(c));
+        }
+    }
+}
